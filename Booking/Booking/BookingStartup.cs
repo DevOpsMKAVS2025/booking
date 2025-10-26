11574d6f8687081fcf7c576687e4ba5a32fb3af8
Milos Stojanovic <mikicamiki.bat@gmail.com>
2025-10-26 15:32:52 +0100

    feat: add services

--- Booking/Booking/BookingStartup.cs
+++ Booking/Booking/BookingStartup.cs
@@ -27,15 +27,17 @@ namespace Booking
             services.AddScoped<IAccommodationService, AccommodationService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IRequestService, RequestService>();
+            services.AddScoped<IRatingService, RatingService>();
         }
         private static void SetupInfrastructure(IServiceCollection services)
         {
             services.AddScoped<IAccommodationRepository, AccommodationRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IRequestRepository, RequestRepository>();
+            services.AddScoped<IRatingRepository, RatingRepository>();
 
             services.AddDbContext<BookingDbContext>(opt =>
-            opt.UseNpgsql("Host=localhost;Port=5432;Database=booking-database;Username=postgres;Password=admin",
+            opt.UseNpgsql("Host=localhost;Port=5432;Database=booking-database;Username=postgres;Password=admin;IncludeErrorDetail=true",
                 x => x.MigrationsHistoryTable("__EFMigrationsHistory", "booking")));
 
             services.AddDbContext<AuthDBContext>(opt =>
